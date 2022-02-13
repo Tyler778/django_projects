e1dc318fa30b66ed86946bb87b3931ff2a5bd0f3
Tyler Q <44939868+Tyler778@users.noreply.github.com>
2022-02-12 19:14:43 -0500

    added detail views for author and list view for author

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -8,7 +8,7 @@ from .models import Book, Author, BookInstance, Genre
 
 def index(request):
     """View function for home page of site."""
-
+    request = request
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
@@ -16,6 +16,9 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
+    #Number of Visits
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     #Genres that Contain the Letter F
     num_genre_f = Genre.objects.filter(name__contains='f').count()
@@ -32,7 +35,8 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genre_f': num_genre_f,
-        'num_books_s': num_books_s
+        'num_books_s': num_books_s,
+        'num_visits': num_visits,
         
     }
 
@@ -53,3 +57,12 @@ class BookListView(generic.ListView):
 #detailView
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+#listAuthors
+class AuthorListView(generic.ListView):
+    model = Author
+
+#detailAuthorView
+class AuthorDetailView(generic.DetailView):
+    model = Author
